ab33066119f0cd502522c6a754530f307dbf9020
Fabrizio Beccaceci <0123fabri@gmail.com>
2024-10-31 16:16:38 +0100

    Init empty spring project

---
+++ src/main/kotlin/com/iplastudio/couple/CoupleBeApplication.kt
@@ -0,0 +1,11 @@
+package com.iplastudio.couple
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CoupleBeApplication
+
+fun main(args: Array<String>) {
+	runApplication<CoupleBeApplication>(*args)
+}
---
+++ src/test/kotlin/com/iplastudio/couple/CoupleBeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.iplastudio.couple
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CoupleBeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
